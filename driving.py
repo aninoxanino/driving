27704369d635bcc25182904d42059aecb34d186c
Chan Hui-Ya <chanhui-ya@ChandeMacBook-Pro.local>
2019-12-31 00:05:25 +0100

    add America

--- driving.py
+++ driving.py
@@ -6,3 +6,12 @@ if nationality == 'Taiwan':
 		print('You can drink and smoke')
 	else:
 		print('You are illegal to drink and smoke')
+elif nationality == 'America' or nationality == 'USA' or nationality == 'US':
+	if age >= 21:
+		print('You can drink and smoke')
+	elif age >= 18 and age < 21:
+		print('You can drink but cannot smoke')
+	else:
+		print('You cannot smoke and drink')
+
+
